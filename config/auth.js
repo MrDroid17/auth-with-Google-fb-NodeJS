cad9e059161a74da50fa38fb49f6a3ed620249bf
Sobhit Kumar <sobhit2010@gmail.com>
2018-07-30 17:24:50 +0530

    add facebook auth

---
+++ config/auth.js
@@ -0,0 +1,24 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth': {
+        'clientID': '283266445562322', // your App ID
+        'clientSecret': '8eec14cef5b76700c061e19a3995322b', // your App Secret
+        'callbackURL': 'http://localhost:8080/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields': ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    },
+
+    'twitterAuth': {
+        'consumerKey': 'your-consumer-key-here',
+        'consumerSecret': 'your-client-secret-here',
+        'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth': {
+        'clientID': 'your-secret-clientID-here',
+        'clientSecret': 'your-client-secret-here',
+        'callbackURL': 'http://localhost:8080/auth/google/callback'
+    }
+
+};
